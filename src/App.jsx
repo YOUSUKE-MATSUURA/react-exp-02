5bca36a0e4d19d4e82f25a8425528b9e6130a6a6
yosuke-matsuura <yo.yo.yousuke0918@gmail.com>
2026-01-18 13:22:21 +0900

    2026.1.18講義

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import News from './components/News'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // inputが2個あるので、入力したものを保持するためにstateを2つ用意する
+  const [name, setName] = useState("名前入力");
+  const [email, setEmail] = useState("メルアドレス入力");
 
+  //イベント処理＝クリックしたら「名前はXXXです」「メルアドレスはXXXです」と表示する
+
+  const handleNameChange = (e) => {
+    //処理を書く
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    //処理を書く
+    setEmail(e.target.value);
+  };
+
+  
+  useEffect(() => {
+    //この中に書きます
+    console.log("起動しました！！！！！");
+    
+    //この下は書かない
+  },[])
+  //useEffectの前に追加でコンソールログを以下に書きます。
+  console.log("順番の確認");
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/*  */}
+      <News />
+       <div>
+        <p>名前が入ります</p>
+        <input 
+        type="text" 
+        placeholder='名前を入力してください' 
+        value={name} onChange={handleNameChange}
+        />
+       </div>
+       <div>
+        <p>メルアドレスが入ります</p>
+        <input 
+        type="text" 
+        placeholder='メルアドレスを入力してください' 
+        value={email} 
+        onChange={handleEmailChange}
+        />
+       </div>
+      {/*  */}
     </>
   )
 }
